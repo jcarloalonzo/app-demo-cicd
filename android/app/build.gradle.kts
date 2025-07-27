250ab1d92b22b18786fb12577cf8227b11cfc00a
Juan Portocarrero <jcarloalonzo@gmail.com>
2025-07-27 18:25:19 -0500

    update cicd

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -111,10 +111,10 @@ android {
         }
         create("production") {
             dimension = "environment"
-            applicationIdSuffix = ".app"
+            applicationIdSuffix = ""
             versionNameSuffix = ""
-            versionCode = 1
-            versionName = "0.0.1"
+            versionCode = 2
+            versionName = "0.0.2"
             //signingConfig = signingConfigs.getByName("releaseProduction")
             buildTypes {
                 getByName("debug") {
