c21629352763a4ea97ddf3be916ed3304d94767f
Juan Portocarrero <jcarloalonzo@gmail.com>
2025-07-29 07:30:22 -0500

    Update cicd

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -106,8 +106,8 @@ android {
             dimension = "environment"
             applicationIdSuffix = ".test"
             versionNameSuffix = "-test"
-            versionCode = 3
-            versionName = "0.0.3"
+            versionCode = 4
+            versionName = "0.0.4"
                     // Mover esto AQUÍ, fuera de buildTypes
             if (keystoreTestPropertiesFile.exists()) {
                 signingConfig = signingConfigs.getByName("releaseStaging")
@@ -129,8 +129,8 @@ android {
             dimension = "environment"
             applicationIdSuffix = ""
             versionNameSuffix = ""
-            versionCode = 5
-            versionName = "0.0.5"
+            versionCode = 6
+            versionName = "0.0.6"
             // Mover esto AQUÍ también
             if (keystoreProdPropertiesFile.exists()) {
                 signingConfig = signingConfigs.getByName("releaseProduction")
