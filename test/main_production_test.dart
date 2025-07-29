1a125df454e596c2ae62cb4192ac1e035ccd75b5
Juan Portocarrero <jcarloalonzo@gmail.com>
2025-07-29 07:47:58 -0500

    update cicd

--- test/main_production_test.dart
+++ test/main_production_test.dart
@@ -1,4 +1,3 @@
-
 // ignore_for_file: avoid_print
 
 import 'package:flutter_test/flutter_test.dart';
--- test/model/user_test.dart
+++ test/model/user_test.dart
@@ -1,4 +1,3 @@
-
 import 'package:app_test_cicd/model/user.dart';
 import 'package:flutter_test/flutter_test.dart';
 
